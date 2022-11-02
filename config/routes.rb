752f7142d24f9d43123bdd3bcc551810ddd9eaed
EC2 Default User <ec2-user@ip-172-31-2-38.ap-northeast-1.compute.internal>
2022-11-02 14:30:31 +0000

    課題4途中

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
      resource :favorites, only: [:create, :destroy]
      resources :book_comments, only: [:create, :destroy]
    end
-  resources :users, only: [:index,:show,:edit,:update,:create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:index,:show,:edit,:update,:create]do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
